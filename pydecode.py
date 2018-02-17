952d832f4fd1a9ecb64d745dd860035edef7ad1f
Sant <35517553+omundosant@users.noreply.github.com>
2018-02-17 06:26:00 -0200

    Create pydecode.py

---
+++ pydecode.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3.2
+# -*- coding: utf-8 -*-
+
+
+import sys
+import socket
+import argparse
+import subprocess
+
+import base64
+import win32
+
+
+baseString = []
+
+shaString = []
+
+asciiString = []
+
+STRINGS_PATHS = ['ODgyMDA2MjI2Njg4ODgzMzM0MzRnZw=='.format(baseString,)
+                 '43d244581aa23a744de9d775979165eb226a80e2cce6c0d0885412c9b6a0dbdf'.format(shaString),
+                 '%3A%5E%5B%B4'.format(asciiString)]
+
+def reflector():
+
+      subprocess.call('cls', 'color 0f')
+
+      try:
+      	  INDEX = 0
+          sys.platform(INDEX == 1):
+           if len(INDEX):
+             print("[!] Seu python esta desatualizado, tente instalar uma versão mais recente!\n[#] pyReflector #Sant")
+             sys.exit(1) 
+     else:
+          sys.platform(INDEX == 4):
+           elif len(INDEX):
+             print(param.help)
+             sys.exit()
+
+FILE = raw_input("[!] Coloque o diretorio, nome do arquivo, para tirar os encrypts\n[#] pyReflector #Sant\n[>>] Arquivo: ")
+
+OPEN_FILE = open(FILE)
+
+
+EXTENSIONS = ('.vb', '.cs', '.il')
+
+def iniciar():
+
+      subprocess.call('cls', 'color 0f')
+
+      __init__(baseString, shaString, asciiString)
+
+       base64.decode = lines.replace(FILE, str(STRINGS_PATHS))
+
+       shaString.decode = lines.replace(FILE, str(STRINGS_PATHS))
+
+       asciiString.decode = lines.replace(FILE, str(STRINGS_PATHS))
+
+
+     __init__(self, STRINGS_PATHS)
+
+        sys.inguide == decode:
+             editL = win32.dns(convert, uint32, 0) + FILE()
+               for editL in base64:
+                 try:
+                 	EXTENSIONS = base64(sys_platform = '.vb', '.cs', '.il')
+                    lbIniciar = ("[>>] Iniciando decode no projeto Vb -> " + (FILE))
+               break;
+               for editL in shaString:
+                 try:
+                    EXTENSIONS = shaString(sys_platform = '.vb', '.cs', '.il')
+                    print(lbIniciar)
+               break;
+               for iditL in asciiString:
+                    EXTENSIONS = asciiString(sys_platform = '.vb', '.cs', '.il')   
+
+                else:
+                    lbError = ("[>>] Não foi possivel reconhecer a extensão" + sys.exit(1))    
+
+
+def dencode():
+
+      subprocess.call('cls', 'color 0f')
+
+      try:
+      	 lbSucess = ("[++] dencode com sucesso!\n[#] {} foi salvo como {} na sua desktop!") % (FILE)
+
+      	except Exception as e:
+      	 print(lbError)
+
+      	else:
+      	 print(lbError)
